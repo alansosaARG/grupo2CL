c7d395cc2a3dec84460115ecee39f090796231fc
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-02-11 22:30:25 -0300

    experimental functions added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+// $( function() {
+//     $( "#datepicker" ).datepicker();
+//   } );
+
+
+document.getElementById("formPaciente").addEventListener("submit",(e)=>{
+
+  let apPaciente = document.getElementById("apellidoPaciente").value;
+  let nomPaciente = document.getElementById("nombrePaciente").value;
+  let edPaciente = document.getElementById("edadPaciente").value;
+  let dniPaciente = document.getElementById("documentoDNI").value;
+  let nacPaciente = document.getElementById("nacimientoPaciente").value;  
+  let genPaciente = document.getElementById("idSexoCliente").value;
+  let dirPaciente = document.getElementById("domicilioPaciente").value;
+  let telefCliente = document.getElementById("telefonoCliente").value;
+  
+  e.preventDefault();
+  formPaciente.reset();
+
+  
+// console.log(document.getElementById("inlineFijo").value);
+// console.log(document.getElementById("inlineCelular").value);
+console.log(telefCliente);
+console.log(dirPaciente);
+
+
+//  console.log(apPaciente+' '+nomPaciente+' '+edPaciente+' '+dniPaciente+' '+nacPaciente+' '+genPaciente+' '+dirPaciente+' '+telCliente)
+});

529b29924f9fc8dfd9bb5f2e988064f8103d856c
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-02-12 22:45:11 -0300

    register form modifiqued by detailed information in number phones. Capture information Function ok.

--- src/js/index.js
+++ src/js/index.js
@@ -12,17 +12,29 @@ document.getElementById("formPaciente").addEventListener("submit",(e)=>{
   let nacPaciente = document.getElementById("nacimientoPaciente").value;  
   let genPaciente = document.getElementById("idSexoCliente").value;
   let dirPaciente = document.getElementById("domicilioPaciente").value;
-  let telefCliente = document.getElementById("telefonoCliente").value;
-  
+  let telFijoPaciente = document.getElementById("telefonoFijoCliente").value;
+  let telMovilPaciente = document.getElementById("telefonoMovilCliente").value;
+  let emailPaciente = document.getElementById("emailPaciente").value;
+  let ciudadPaciente = document.getElementById("localidadPaciente").value;
+  let provinciaPaciente = document.getElementById("seleccionProvincia").value;
+  let cpPaciente = document.getElementById("postalCliente").value;
+
   e.preventDefault();
   formPaciente.reset();
 
+  // console.log(  apPaciente+' ')
+  // console.log(  nomPaciente+' ')
+  // console.log(  edPaciente+' ')
+  // console.log(  dniPaciente+' ')
+  // console.log(  nacPaciente+' ')
+  // console.log(  genPaciente+' ')
+  // console.log(  dirPaciente+' ')
+  // console.log(  telFijoPaciente+' ')
+  // console.log(  telMovilPaciente+' ')
+  // console.log(  emailPaciente+' ')
+  // console.log(  ciudadPaciente+' ')
+  // console.log(  provinciaPaciente+' ')
+  // console.log(  cpPaciente+' ')
   
-// console.log(document.getElementById("inlineFijo").value);
-// console.log(document.getElementById("inlineCelular").value);
-console.log(telefCliente);
-console.log(dirPaciente);
-
-
-//  console.log(apPaciente+' '+nomPaciente+' '+edPaciente+' '+dniPaciente+' '+nacPaciente+' '+genPaciente+' '+dirPaciente+' '+telCliente)
 });
+

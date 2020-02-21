3c877b15a6ae210efb8f68a0a9469432bf44947b
Fredy <fredykv@hotmail.com>
2020-02-20 22:35:45 -0300

    create a doctores and pacientes class

---
+++ src/js/pacientes.js
@@ -0,0 +1,15 @@
+class Pacientes
+ {
+    constructor (nombre){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.telefono = telefono,
+        this.edad = edad,
+        this.correo = correo,
+        this.domiciolio = domiciolio,
+        this.provincia = provincia,
+        this.localidad = localiad,
+        this.codigopostal = codigopostal,
+        this.celular = celular
+    }
+}

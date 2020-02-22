ccbb2b5076eb1b3ff78b66afeca652e148b4fdf7
marcobaiad <marcobaiad@gmail.com>
2020-02-22 17:41:13 -0300

    add code js

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+function getRegisterD() {
+  const inputs = document.querySelectorAll('datos')
+  registro = {}
+  inputs.forEach((dato) => registro[dato.id] = dato.value)
+}
+const doctor = new doctor(registro.nombre, )
+
+for (let index = 0; index < inputs.length; index++) {
+  const dato = inputs[index];
+  
+}
